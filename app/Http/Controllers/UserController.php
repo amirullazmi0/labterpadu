523d7e40edf1e8196850556dd93f5700c3ace9b8
Amirul Azmi <amirullazmi0@gmail.com>
2023-01-20 18:37:23 +0700

    20 januari 2023, 18.36

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Lab;
+use App\Models\Alat;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
     //
+    public function index()
+    {
+        $j_alat = Alat::where('lab_id', '=', auth()->user()->lab_id)->count();
+
+        return view('/admin/index', [
+            "title" => "Halaman Dashboard",
+            "active" => "dashboard",
+            "nomor" => 1,
+            "lab" => Lab::all(),
+            "user" => User::all(),
+            "jumlah_alat" => $j_alat,
+        ]);
+    }
 }
